5cb806e4d06124aac426f333cf6bd4ada6f68042
RK <robertkozak97@gmail.com>
2022-12-05 12:59:17 +0100

    Binary search alogithms + Simply test case

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,32 @@
 
+#list = [1,2,3,4,5]
+#value = [4]
+#answer = 2
+from math import ceil, floor
+
 def binary_search(list, value):
-    return 5
+    left_index = 0
+    right_index = len(list) - 1   
+    
+
+    while left_index <= right_index:
+        
+        medium_index = (left_index + right_index) // 2    
+      
+        if value > list[medium_index]:
+            left_index = medium_index + 1
+     
+        elif value < list[medium_index]:
+            right_index = medium_index - 1
+   
+        else:            
+            return medium_index
+
+    return None
+    
+        
 
 
+
+
+print(binary_search([1,2,3,4,5,6,7,8], 1)) # [3] -> 4, 
--- tests.py
+++ tests.py
@@ -4,8 +4,8 @@ from binary_search import binary_search
 class TestAlgorithms(unittest.TestCase):
     
     def test_binary_Search(self):        
-        self.assertEqual(binary_search([1,2,3,4,5,6,7,8], 5), 5, 4)
-
+        self.assertEqual(binary_search([1,2,3,4,5,6,7,8], 9), None)
+        
 
 if __name__ == "__main__":
     unittest.main()
