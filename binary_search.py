0a7d8b52006ae9f62e2f290928ba4a5dbfc4aff5
unknown <robertkozak97@gmail.com>
2023-12-08 17:48:28 +0100

    Binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,24 @@
+input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 15, 16, 21, 24, 45]
+wanted = 5
 
-def binary_search(list, value):
-    return 5
+
+def binary_search(input, wanted):
+    low = 0
+    high = len(input) - 1
+    n = 1
+    while low <= high:
+        mid = -(-(low + high) // 2)
+        print(mid)
+        check = input[mid]
+        if check < wanted:
+            low = mid + 1
+        elif check > wanted:
+            high = mid - 1
+        else:
+            print(n)
+            return mid
+        n += 1
+    return -1
 
 
+print(binary_search(input, wanted))
